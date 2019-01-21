4788210b903105260e6c6188fbcf959c4454f8a6
anhdroid <p.anh96@gmail.com>
2019-01-21 14:07:55 +0100

    add metrics

--- dashboard/app.py
+++ dashboard/app.py
@@ -72,6 +72,13 @@ example_metrics_db = {
 }
 
 global data_metrics_storm
+data_metrics_storm = {
+    'averagePerMinute': [0],
+    'time':[datetime.datetime.now()],
+    'averageMsSystem': 0
+}
+
+
 global data_metrics_db
 
 
@@ -83,7 +90,7 @@ def updateSum(val):
 
 
 def callback(ch, method, properties, body):
-    #print(" [x] Received %r" % json.loads(body))
+    print(" [x] Received %r" % json.loads(body))
     updateSum(json.loads(body))
 
 channel.basic_consume(callback, queue_name, no_ack=True)
@@ -93,9 +100,7 @@ channel.basic_consume(callback, queue_name, no_ack=True)
 def updateCat(category):
     for cat, val in category.items():
         data_cat[cat] = val
-    #print(data_cat)
-    #df = pd.DataFrame.from_dict(data_cat)
-    #print(df)
+
 
 
 def callback_cat(ch, method, properties, body):
@@ -105,39 +110,61 @@ def callback_cat(ch, method, properties, body):
 
 channel_cat.basic_consume(callback_cat, queue_name_cat, no_ack=True)
 
+def updateStorm(data):
+    data_metrics_storm['time'].append(datetime.datetime.now())
+    data_metrics_storm['averagePerMinute'].append(data['averagePerMinute'])
+    data_metrics_storm['averageMsSystem'] = data['averageMsSystem']
+
+
+def callback_storm(ch, method, properties, body):
+    updateStorm(body)
+
+channel_metrics_storm.basic_consume(callback_storm,queue_name_metrics_storm, no_ack=True)
+
 
 app.layout = html.Div(
     html.Div([
         html.H4('Sales Live Feed'),
         html.Div(id='live-update-text'),
-        dcc.Graph(id='live-update-graph'),
+        dcc.Graph(id='live-update-graph',animate=True),
+        dcc.Interval(
+            id='interval-component-graph',
+            interval=2000, # in milliseconds
+            n_intervals=0
+        ),
         dcc.Interval(
             id='interval-component',
             interval=1000, # in milliseconds
             n_intervals=0
-        )
+        ),
+
+
     ])
+
 )
 
 @app.callback(Output('live-update-text', 'children'),
               [Input('interval-component', 'n_intervals')])
 def interfaceSum(n):
     #data = np.load('dataSum.npy').item()
-    style = {'padding': '5px', 'fontSize': '16px'}
+    style = {'padding': '20px', 'fontSize': '24px'}
     return [
-        html.Span('Chiffre d\'affaire: {}'.format(data['sum'][-1]), style=style)
+        html.Span('Daily revenue: {}'.format(data['sum'][-1]), style=style),
+        html.Span('Number of receipts: {}'.format(data['count']), style=style),
+        html.Span('Average ingested/min : {}'.format(data_metrics_storm['averageMsSystem']), style=style)
+
     ]
 
 @app.callback(Output('live-update-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
+               [Input('interval-component', 'n_intervals')])
 def display(n):
     #data_cat = np.load('dataCat.npy').item()
     #print(data_cat)
-    fig = plotly.tools.make_subplots(rows=2, cols=1, vertical_spacing=0.2)
+    fig = plotly.tools.make_subplots(rows=2, cols=2, vertical_spacing=0.2)
     fig['layout']['margin'] = {
-        'l': 30, 'r': 10, 'b': 30, 't': 10
+        'l': 30, 'r': 10, 'b': 30, 't': 30
     }
-    fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
+    fig['layout']['legend'] = {'x': 1, 'y': 0, 'xanchor': 'right'}
 
     fig.append_trace({
         'x': data['time'],
@@ -155,14 +182,23 @@ def display(n):
         'type': 'bar'
     }, 2, 1)
 
+    fig.append_trace({
+        'x': data_metrics_storm['time'],
+        'y': data_metrics_storm['averagePerMinute'],
+        'name': 'Avg receipts ingested/min',
+        'mode': 'lines+markers',
+        'type': 'scatter'
+    }, 1, 2)
+
     return fig
 
 
 def start_multi():
-    executor = ThreadPoolExecutor(max_workers=3)
+    executor = ProcessPoolExecutor(max_workers=4)
 
     executor.submit(channel.start_consuming)
     executor.submit(channel_cat.start_consuming)
+    executor.submit(channel_metrics_storm.start_consuming)
     executor.submit(app.run_server(host='0.0.0.0',debug=True, port=8050))
 
 if __name__ == '__main__':
